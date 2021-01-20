8a526e716a41e74274b9e929591a5ad56fa3e529
Annie <annieholland@myop.ac.nz>
2021-01-20 21:51:52 +1300

    Added my favourite mode of transport

--- src/partD/Main.java
+++ src/partD/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hi, my name is Annie");
 		System.out.println("My favourite colour is yellow");
+		System.out.println("My favourite mode of transport is car");
 	}
 
 }
