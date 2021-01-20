ba87f7222efe1860064e50cea13a8e2b77628463
Annie <annieholland@myop.ac.nz>
2021-01-20 21:52:55 +1300

    Added my favourite day of the week

--- src/partD/Main.java
+++ src/partD/Main.java
@@ -6,6 +6,7 @@ public class Main {
 		System.out.println("Hi, my name is Annie");
 		System.out.println("My favourite colour is yellow");
 		System.out.println("My favourite mode of transport is car");
+		System.out.println("The best day of the week is payday...kidding, Saturday");
 	}
 
 }
